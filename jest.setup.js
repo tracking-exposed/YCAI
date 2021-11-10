573f8c0b2646896baa0d3bedb9a3482190c3ba57
ascariandrea <dev.ascariandrea@gmail.com>
2021-11-10 16:59:43 +0100

    chore: e2e tests with puppeteer

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,13 @@
+// load the content of .env file in process.env
+require('dotenv/config')
 const { chrome } = require('jest-chrome/lib/index.cjs');
 const { SVGPathElement } = require('svgdom');
 
 process.env.REACT_APP_BUILD_DATE = new Date().toISOString();
 process.env.REACT_APP_VERSION = '0.1-TEST';
 
+// patch global object with chrome api
+const { chrome } = require('jest-chrome/lib/index.cjs');
 Object.assign((global.chrome = {}), chrome);
 Object.assign((global.browser = {}), chrome);
 
