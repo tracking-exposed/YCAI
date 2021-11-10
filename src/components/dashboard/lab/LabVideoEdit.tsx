1f7a917db9dfa2866587d4196a95313b59e91073
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-11-10 17:41:33 +0100

    feat: add title on lab edit page

--- src/components/dashboard/lab/LabVideoEdit.tsx
+++ src/components/dashboard/lab/LabVideoEdit.tsx
@@ -2,7 +2,6 @@ import { Grid } from '@material-ui/core';
 import { YTVideo } from 'components/common/YTVideo';
 import * as React from 'react';
 import AddRecommendationBox from '../AddRecommendationBox';
-import { Recommendations } from '../Recommendations';
 import { VideoRecommendations } from '../VideoRecommendationsEdit';
 
 interface LabVideoEditProps {
@@ -18,10 +17,6 @@ export const LabVideoEdit: React.FC<LabVideoEditProps> = ({
       <Grid item md={6} sm={5}>
         <YTVideo videoId={videoId} />
         <AddRecommendationBox />
-        <Recommendations
-          queries={{ videoRecommendations: { videoId } }}
-          videoId={videoId}
-        />
       </Grid>
       <Grid item md={4}>
         <VideoRecommendations
