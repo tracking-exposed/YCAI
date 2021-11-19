76d8a355b9f4c0267a681433f606c7faa77426ac
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-11-19 15:06:24 +0100

    fix: added env variable for tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,7 @@ const { chrome } = require('jest-chrome/lib/index.cjs');
 const { SVGPathElement } = require('svgdom');
 
 process.env.REACT_APP_BUILD_DATE = new Date().toISOString();
+process.env.REACT_APP_VERSION = '0.1-TEST';
 
 Object.assign((global.chrome = {}), chrome);
 Object.assign((global.browser = {}), chrome);
