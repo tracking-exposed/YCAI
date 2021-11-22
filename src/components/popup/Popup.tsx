e47e26feef89d2aac0c682b342ebfd493871e9f6
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-11-22 14:34:12 +0100

    fix: align switches to top & cleanup

--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,9 +1,9 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
-  Grid,
   makeStyles,
   Typography,
 } from '@material-ui/core';
@@ -79,18 +79,20 @@ export const Popup: React.FC = () => {
 
   return (
     <Card className={classes.container}>
-      <Grid className={classes.header} container alignItems="center" align="center">
-        <Grid item xs={12}>
-          <a
-            className={classes.link}
-            href={config.REACT_APP_WEB_URL}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img className={classes.img} src="/ycai-logo.svg" />
-          </a>
-        </Grid>
-      </Grid>
+      <Box
+        className={classes.header}
+        display="flex"
+        justifyContent="center"
+      >
+        <a
+          className={classes.link}
+          href={config.REACT_APP_WEB_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className={classes.img} src="/ycai-logo.svg" />
+        </a>
+      </Box>
 
       <WithQueries
         queries={{ settings }}
@@ -102,7 +104,13 @@ export const Popup: React.FC = () => {
           ({ settings }): any => {
             const content = (
               <CardContent className={classes.content}>
-                <Grid key="app-version" className={classes.version} item xs={12} align="center">
+                <Box
+                  className={classes.version}
+                  alignItems="center"
+                  display="flex"
+                  justifyContent="center"
+                  key="app-version"
+                >
                   <Typography variant="caption">
                     {t('popup:version', { version, date: timeago })}
                   </Typography>
@@ -113,13 +121,13 @@ export const Popup: React.FC = () => {
                       display="inline"
                       style={{
                         fontWeight: 800,
+                        marginBottom: 0,
                       }}
                     >
-                      {' '}
-                      DEVELOPMENT
+                      &nbsp;DEVELOPMENT
                     </Typography>
                   ) : null}
-                </Grid>
+                </Box>
 
                 <Settings key="settings" settings={settings} />
               </CardContent>
--- src/components/popup/Settings.tsx
+++ src/components/popup/Settings.tsx
@@ -6,7 +6,6 @@ import {
   makeStyles,
   Switch,
   Typography,
-  /*useTheme,*/
 } from '@material-ui/core';
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -22,7 +21,8 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 30,
     paddingBottom: 12,
   },
-  noMargin: {
+  controlLabel: {
+    alignItems: 'flex-start',
     marginLeft: 0,
     marginRight: 0,
     marginBottom: theme.spacing(2),
@@ -39,14 +39,11 @@ interface SettingsProps {
 const Settings: React.FC<SettingsProps> = ({ settings }) => {
   const { t } = useTranslation();
   const classes = useStyles();
-  /* const theme = useTheme(); */
 
   return (
     <>
-      {/* <FormHelperText className={classes.smallTitle}>{t('recommendations:title')}</FormHelperText> */}
-      <br />
       <FormControlLabel
-        className={classes.noMargin}
+        className={classes.controlLabel}
         disabled={!settings.active}
         control={
           <Switch
@@ -64,7 +61,7 @@ const Settings: React.FC<SettingsProps> = ({ settings }) => {
             <Typography variant="h5">
               {t('settings:contentCreatorRecommendationLabel')}
             </Typography>
-            <Typography variant="body" display="block">
+            <Typography display="block">
               {t('settings:contentCreatorRecommendationHint')}
             </Typography>
             <br />
@@ -75,46 +72,10 @@ const Settings: React.FC<SettingsProps> = ({ settings }) => {
         }
         labelPlacement="end"
       />
-      {/*
-      <br />
-      <br />
-      <FormControlLabel
-        className={classes.noMargin}
-        disabled={true}
-        control={
-          <Switch
-            className={classes.marginRight}
-            aria-labelledby="switch-recommendations"
-            color="primary"
-            checked={settings.communityRecommendations}
-            onChange={(e, checked) =>
-              updateSettings({
-                ...settings,
-                communityRecommendations: checked,
-              })()
-            }
-          />
-        }
-        label={
-          <FormLabel>
-            <Typography variant="h6">
-              {t('settings:communityRecommendationsLabel')}
-            </Typography>
-            <Typography variant="body2" display="block">
-              {t('settings:communityRecommendationsHint')}
-            </Typography>
-          </FormLabel>
-        }
-        labelPlacement="end"
-      />
-      <br />
-      <br />       
-      <Divider className={classes.divider} /> */}
-
 
       <FormControl component="fieldset">
         <FormControlLabel
-          className={classes.noMargin}
+          className={classes.controlLabel}
           disabled={!settings.active}
           control={
             <Switch
@@ -142,7 +103,7 @@ const Settings: React.FC<SettingsProps> = ({ settings }) => {
               <Typography variant="h5">
                 {t('settings:contributeToIndependentStatsLabel')}
               </Typography>
-              <Typography variant="body" display="block">
+              <Typography display="block">
                 {t('settings:contributeToIndependentStatsHint')}
               </Typography>
               <br />
@@ -151,56 +112,6 @@ const Settings: React.FC<SettingsProps> = ({ settings }) => {
           }
           labelPlacement="end"
         />
-        {/*
-        {settings.independentContributions.enable ? (
-          <FormControl style={{ paddingLeft: 80 }}>
-            <FormControlLabel
-              control={
-                <Switch
-                  size="small"
-                  aria-labelledby="switch-independentContributions-showUI"
-                  color="primary"
-                  checked={settings.independentContributions.showUI}
-                  onChange={(e, showUI) => {
-                    void updateSettings({
-                      ...settings,
-                      independentContributions: {
-                        ...settings.independentContributions,
-                        showUI,
-                      },
-                    })();
-                  }}
-                />
-              }
-              label={
-                <FormLabel>
-                  <Typography
-                    variant="subtitle1"
-                    style={{ marginBottom: theme.spacing(2) }}
-                  >
-                    {t('settings:contributeToIndependentStatsShowUILabel')}
-                  </Typography>
-                  <Typography variant="body2" display="block">
-                    {t('settings:contributeToIndependentStatsShowUIHint')}
-                  </Typography>
-                  <br />
-                </FormLabel>
-              }
-            />
-            
-            <Box>
-              <Button
-                color="primary"
-                variant="outlined"
-                size="small"
-                href="/index.html?path=/settings/"
-                target="_blank"
-              >
-                {t('actions:manage_tokens')}
-              </Button>
-            </Box>
-          </FormControl>
-        ) : null} */}
       </FormControl>
     </>
   );
